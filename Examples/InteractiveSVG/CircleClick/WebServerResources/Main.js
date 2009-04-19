58d19d25461077b8ff8645fa28bce174fc6a0ac2
mendismeister <mendismeister@2cbf19bc-9704-11dd-93bf-7bb45232ab92>
2009-04-19 01:53:43 +0000

    Basic CicleClick example using jQuery for client-side interactivity

--- Examples/InteractiveSVG/CircleClick/WebServerResources/Main.js
+++ Examples/InteractiveSVG/CircleClick/WebServerResources/Main.js
@@ -1,44 +1,10 @@
 $(function() {
 	$('#svgbasics').svg({onLoad: drawInitial});
-	$('button').click(drawShape);
 });
 
 function drawInitial(svg) {
-	svg.circle(75, 75, 50, {fill: 'none', stroke: 'red', 'stroke-width': 3});
-	var g = svg.group({stroke: 'black', 'stroke-width': 2});
-	svg.line(g, 15, 75, 135, 75);
-	svg.line(g, 75, 15, 75, 135);
-}
-
-var colours = ['purple', 'red', 'orange', 'yellow', 'lime', 'green', 'blue', 'navy', 'black'];
-
-function drawShape() {
-	var shape = this.id;
-	var svg = $('#svgbasics').svg('get');
-	if (shape == 'rect') {
-		svg.rect(random(300), random(200), random(100) + 100, random(100) + 100,
-			{fill: colours[random(9)], stroke: colours[random(9)],
-			'stroke-width': random(5) + 1});
-	}
-	else if (shape == 'line') {
-		svg.line(random(400), random(300), random(400), random(300),
-			{stroke: colours[random(9)], 'stroke-width': random(5) + 1});
-	}
-	else if (shape == 'circle') {
-		svg.circle(random(300) + 50, random(200) + 50, random(80) + 20,
-			{fill: colours[random(9)], stroke: colours[random(9)],
-			'stroke-width': random(5) + 1});
-	}
-	else if (shape == 'ellipse') {
-		svg.ellipse(random(300) + 50, random(200) + 50, random(80) + 20, random(80) + 20,
-			{fill: colours[random(9)], stroke: colours[random(9)],
-			'stroke-width': random(5) + 1});
-	}
-	else if (shape == 'clear') {
-		svg.clear();
-	}
-}
-
-function random(range) {
-	return Math.floor(Math.random() * range);
+	svg.describe('Example script01 - invoke an ECMAScript function from an onclick event'); 
+	svg.script('function circle_click(evt) {\n' + '  var circle = evt.target;\n' + '  var currentRadius = circle.getAttribute("r");\n' + '  if (currentRadius == 100)\n' + '    circle.setAttribute("r", currentRadius*2);\n' + '  else\n' + '    circle.setAttribute("r", currentRadius*0.5);\n' + '}', 'text/ecmascript'); 
+	svg.circle(300, 150, 100, {onclick: 'circle_click(evt)', fill: 'red'}); 
+	svg.text(300, 280, 'Click on circle to change its size', {'font-family': 'Verdana', 'font-size': 35, 'text-anchor': 'middle'}); 
 }
